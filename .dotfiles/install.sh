cd389ac4091442a7227a8d16448ff97dea46c99e
Jo Santana <josantana@jogalabs.com>
2014-09-17 12:27:22 -0300

    Completed Install and Sync scripts
    - Fixed Homebrew condition to install
    - Sync only install dotfiles related to Terminal

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -4,15 +4,14 @@
 mv ~/.bash_profile ~/.dotbackup/.bash_profile && mv ~/.bashrc ~/.dotbackup/.bashrc_backup
 
 # Clone repository to user's root folder
-cd; curl --silent -#L "https://github.com/josantana/dotfiles/tarball/master" | tar -xzv --strip-components 1 --exclude={LICENSE,README.md,install.sh}
+cd; curl --silent -#L "https://github.com/josantana/dotfiles/tarball/master" | tar -xzv --strip-components 1 --exclude={LICENSE,README.md,install.sh,sync.sh}
 
-# Z directory jumping (from https://github.com/rupa/z)
+# Z directory jumping (https://github.com/rupa/z)
 curl -O "https://raw.githubusercontent.com/rupa/z/master/z.sh" && mv z.sh ~/.dotfiles/z.sh
 
 # Homebrew for the homeless
-has_brew = which brew
-if ["$has_brew" == "brew not found"]; then
-    ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go/install)
+if [ -z "$(which brew)" ]; then
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 fi
 
 # Run OSX scripts
---
+++ .dotfiles/sync.sh
@@ -0,0 +1,10 @@
+## SYNC
+
+# Backing up previous Bash Profile
+mv ~/.bash_profile ~/.dotbackup/.bash_profile && mv ~/.bashrc ~/.dotbackup/.bashrc_backup
+
+# Clone repository to user's root folder
+cd; curl --silent -#L "https://github.com/josantana/dotfiles/tarball/master" | tar -xzv --strip-components 1 --exclude={LICENSE,README.md,install.sh,sync.sh}
+
+# Z directory jumping (https://github.com/rupa/z)
+curl -O "https://raw.githubusercontent.com/rupa/z/master/z.sh" && mv z.sh ~/.dotfiles/z.sh
